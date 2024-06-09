c50cd4c680c7c65b431c8baa0f68ba57ba5270d6
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-09 17:53:56 -0300

    build: create project

---
+++ src/main/java/code/blurone/leashablefences/LeashableFences.kt
@@ -0,0 +1,14 @@
+package code.blurone.leashablefences
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class LeashableFences : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
